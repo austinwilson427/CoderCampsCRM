c3fc10563a81849144265c1373f5a1f81036e50b
bandic22 <bandic22@gmail.com>
2016-02-06 15:02:50 -0800

    DuranBranch3

--- CoderCampsCRM/CoderCampsCRM/ngApp/controllers/contactListController.ts
+++ CoderCampsCRM/CoderCampsCRM/ngApp/controllers/contactListController.ts
@@ -4,18 +4,20 @@
 
         public contacts;
 
-        constructor(private contactService: MyApp.Services.ContactService, private $location: ng.ILocationService) {  
-                
+        constructor(private contactService: MyApp.Services.ContactService, private $location: ng.ILocationService, private $uibModal: ng.ui.bootstrap.IModalService) {         
             this.contacts = contactService.getAllContacts();
         }
 
         public openNewContactModal() {
-            
-            
+            this.$uibModal.open({
+                templateUrl: "/ngApp/views/modals/contactAddModal.html",
+                controller: MyApp.Controllers.ContactAddController,
+                controllerAs: 'vm',
+                size: "md"
+            })        
         }
 
         public openContactDetailsPage(id: number) {
-
             this.$location.path('/contactList/' + id);
         }
     }
