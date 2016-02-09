b8294d20bad9c9f09f9c6030fcbf7d82ef186f0c
mcollins2618 <mcollins2618@gmail.com>
2016-02-09 14:25:48 -0800

    taskController

---
+++ CoderCampsCRM/CoderCampsCRM/ngApp/controllers/taskConroller.js
@@ -0,0 +1,66 @@
+var MyApp;
+(function (MyApp) {
+    var Controllers;
+    (function (Controllers) {
+        var TaskListController = (function () {
+            function TaskListController(taskService) {
+                this.tasks = taskService.listTasks();
+            }
+            return TaskListController;
+        })();
+        Controllers.TaskListController = TaskListController;
+        var TaskAddController = (function () {
+            function TaskAddController(taskService, $location) {
+                this.taskService = taskService;
+                this.$location = $location;
+            }
+            TaskAddController.prototype.addScore = function () {
+                var _this = this;
+                this.loaded = false;
+                this.taskService.saveTask(this.taskToAdd).then(function () {
+                    _this.loaded = true;
+                    _this.$location.path("/tasks");
+                });
+            };
+            TaskAddController.prototype.cancelAdd = function () {
+                this.$location.path("/tasks");
+            };
+            return TaskAddController;
+        })();
+        Controllers.TaskAddController = TaskAddController;
+        var TaskEditController = (function () {
+            function TaskEditController(taskService, $location, $routeParams) {
+                this.taskService = taskService;
+                this.$location = $location;
+                this.taskToEdit = this.taskService.getTask($routeParams["id"]);
+            }
+            TaskEditController.prototype.editScore = function () {
+                var _this = this;
+                this.taskService.saveTask(this.taskToEdit).then(function () {
+                    _this.$location.path("/tasks");
+                });
+            };
+            TaskEditController.prototype.cancelEdit = function () {
+                this.$location.path("/tasks");
+            };
+            return TaskEditController;
+        })();
+        Controllers.TaskEditController = TaskEditController;
+        var TaskDeleteController = (function () {
+            function TaskDeleteController(taskService, $location, $routeParams) {
+                this.taskService = taskService;
+                this.$location = $location;
+                this.taskToDelete = taskService.getTask($routeParams["id"]);
+            }
+            TaskDeleteController.prototype.deleteTask = function () {
+                var _this = this;
+                this.taskService.deleteTask(this.taskToDelete.id).then(function () { return _this.$location.path("/tasks"); });
+            };
+            TaskDeleteController.prototype.cancelDelete = function () {
+                this.$location.path("/tasks");
+            };
+            return TaskDeleteController;
+        })();
+        Controllers.TaskDeleteController = TaskDeleteController;
+    })(Controllers = MyApp.Controllers || (MyApp.Controllers = {}));
+})(MyApp || (MyApp = {}));
---
+++ CoderCampsCRM/CoderCampsCRM/ngApp/services/companyService.js
@@ -0,0 +1,30 @@
+var MyApp;
+(function (MyApp) {
+    var Services;
+    (function (Services) {
+        var CompaniesService = (function () {
+            function CompaniesService($resource) {
+                this.$resource = $resource;
+                this.companiesResource = $resource("/api/companies/:id");
+            }
+            CompaniesService.prototype.getCompanies = function () {
+                return this.companiesResource.query();
+            };
+            CompaniesService.prototype.getCompany = function (id) {
+                return this.companiesResource.get({ id: id });
+            };
+            CompaniesService.prototype.createCompany = function (company) {
+                return this.companiesResource.save(company).$promise;
+            };
+            CompaniesService.prototype.deleteCompany = function (id) {
+                return this.companiesResource.delete({ id: id }).$promise;
+            };
+            CompaniesService.prototype.editCompany = function (company) {
+                return this.companiesResource.save(company).$promise;
+            };
+            return CompaniesService;
+        })();
+        Services.CompaniesService = CompaniesService;
+        angular.module("MyApp").service("companiesService", CompaniesService);
+    })(Services = MyApp.Services || (MyApp.Services = {}));
+})(MyApp || (MyApp = {}));
