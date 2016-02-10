9e7e499489b64d3d2d4ebb99bd775d4d3d4c8977
mcollins2618 <mcollins2618@gmail.com>
2016-02-10 15:20:40 -0800

    commit

--- CoderCampsCRM/CoderCampsCRM/ngApp/controllers/contactListController.js
+++ CoderCampsCRM/CoderCampsCRM/ngApp/controllers/contactListController.js
@@ -25,3 +25,4 @@ var MyApp;
         Controllers.ContactListController = ContactListController;
     })(Controllers = MyApp.Controllers || (MyApp.Controllers = {}));
 })(MyApp || (MyApp = {}));
+//# sourceMappingURL=contactListController.js.map
