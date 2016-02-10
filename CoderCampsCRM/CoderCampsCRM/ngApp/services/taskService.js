840f1e89d6793e8c92c7634e4937b7c68510c4ce
bandic22 <bandic22@gmail.com>
2016-02-10 09:20:03 -0800

    Duran

--- CoderCampsCRM/CoderCampsCRM/ngApp/services/taskService.js
+++ CoderCampsCRM/CoderCampsCRM/ngApp/services/taskService.js
@@ -28,3 +28,4 @@ var MyApp;
         angular.module("MyApp").service("taskService", TaskService);
     })(Services = MyApp.Services || (MyApp.Services = {}));
 })(MyApp || (MyApp = {}));
+//# sourceMappingURL=taskService.js.map
