0453c49c5883cf2c80378a2b09b1d44ff8616cf9
austinwilson427 <austin.wilson427@gmail.com>
2016-02-10 07:20:29 -0600

    Commit

--- CoderCampsCRM/CoderCampsCRM/ngApp/controllers/dealController.js
+++ CoderCampsCRM/CoderCampsCRM/ngApp/controllers/dealController.js
@@ -3,7 +3,7 @@ var MyApp;
     var Controllers;
     (function (Controllers) {
         var DealsController = (function () {
-            function DealsController(dealService, $uibModal, $location, $route) {
+            function DealsController(dealService, $uibModal, $location, $route, companiesService) {
                 this.dealService = dealService;
                 this.$uibModal = $uibModal;
                 this.$location = $location;
@@ -165,7 +165,7 @@ var MyApp;
                             }
                             else if (today_month == inner_month && today_date == inner_date && today_year == inner_year) {
                                 filteredDates.push(result[i_1]);
-                        }
+                            }
                         }
                         else if (_this.dateFilter == "month") {
                             if (inner_set < month_from_today && inner_full_num >= today_num) {
@@ -173,7 +173,7 @@ var MyApp;
                             }
                             else if (today_month == inner_month && today_date == inner_date && today_year == inner_year) {
                                 filteredDates.push(result[i_1]);
-                        }
+                            }
                         }
                         else {
                             filteredDates.push(result[i_1]);
