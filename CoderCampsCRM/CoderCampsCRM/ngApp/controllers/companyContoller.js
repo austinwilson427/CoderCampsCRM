aea97971524ee6af407a4104ff21416c1e5a074c
mcollins2618 <mcollins2618@gmail.com>
2016-02-12 11:09:52 -0800

    configuration fix

--- CoderCampsCRM/CoderCampsCRM/ngApp/controllers/companyContoller.js
+++ CoderCampsCRM/CoderCampsCRM/ngApp/controllers/companyContoller.js
@@ -90,3 +90,4 @@ var MyApp;
         Controllers.EditCompanyController = EditCompanyController;
     })(Controllers = MyApp.Controllers || (MyApp.Controllers = {}));
 })(MyApp || (MyApp = {}));
+//# sourceMappingURL=companyContoller.js.map
--- CoderCampsCRM/CoderCampsCRM/ngApp/services/contactService.js
+++ CoderCampsCRM/CoderCampsCRM/ngApp/services/contactService.js
@@ -34,3 +34,4 @@ var MyApp;
         angular.module("MyApp").service("contactService", ContactService);
     })(Services = MyApp.Services || (MyApp.Services = {}));
 })(MyApp || (MyApp = {}));
+//# sourceMappingURL=contactService.js.map
