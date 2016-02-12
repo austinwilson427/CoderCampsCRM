ee9c7b173f60558b15ab801c593ef2deb2559023
Austin Wilson <austin.wilson427@gmail.com>
2016-02-12 02:25:26 +0000

    Added 3 files to services

---
+++ CoderCampsCRM/CoderCampsCRM/ngApp/services/dealLogItemService.ts
@@ -0,0 +1,39 @@
+﻿namespace MyApp.Services {
+
+    export class DealLogItemService {
+        
+        private dealResource;
+        private dealResourceFromDeal;
+
+        constructor(private $resource: ng.resource.IResourceService) {
+            this.dealResource = $resource("api/deallogitems/:id");
+            this.dealResourceFromDeal = $resource("api/deallogitems/deal/:id");
+        }
+
+        public listAllDealLogItems() {
+            return this.dealResource.query();
+        }
+
+        public getDealLogItemsById(id) {
+            return this.dealResource.get({ id: id });
+        }
+
+        public listDealLogItemsByDealId(id) {
+            return this.dealResourceFromDeal.query({ id: id });
+        }
+
+        public saveDealLogItem(dealToSave) {
+            return this.dealResource.save(dealToSave).$promise;
+        }
+
+        public deleteDealLogItem(id) {
+            return this.dealResource.delete({ id: id }).$promise;
+        }
+
+
+
+    }
+
+    angular.module("MyApp").service("dealLogItemService", DealLogItemService);
+
+}
