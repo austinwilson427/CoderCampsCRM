64b6e908aa52a5f551e0983e301bd466e8a8b3ff
Austin Wilson <austin.wilson427@gmail.com>
2016-02-12 23:52:02 +0000

    Added 2 files to services

---
+++ CoderCampsCRM/CoderCampsCRM/ngApp/services/dealContactService.js
@@ -0,0 +1,27 @@
+var MyApp;
+(function (MyApp) {
+    var Services;
+    (function (Services) {
+        var DealContactService = (function () {
+            function DealContactService($resource) {
+                this.$resource = $resource;
+                this.dealContactResource = $resource("api/dealcontacts/:id");
+            }
+            DealContactService.prototype.getAllDealContacts = function () {
+                return this.dealContactResource.query();
+            };
+            DealContactService.prototype.getAllDealContactsByDealId = function (id) {
+                return this.dealContactResource.query({ id: id });
+            };
+            DealContactService.prototype.saveDealContact = function (contactToSave) {
+                return this.dealContactResource.save(contactToSave).$promise;
+            };
+            DealContactService.prototype.deleteDealContact = function (id) {
+                return this.dealContactResource.delete({ id: id }).$promise;
+            };
+            return DealContactService;
+        })();
+        Services.DealContactService = DealContactService;
+        angular.module("MyApp").service("dealContactService", DealContactService);
+    })(Services = MyApp.Services || (MyApp.Services = {}));
+})(MyApp || (MyApp = {}));
