ca4266f1cb38844b9d4bfecf1b2b56aedc31b5ac
mcollins2618 <mcollins2618@gmail.com>
2016-02-17 15:10:24 -0800

    task config fix

--- CoderCampsCRM/CoderCampsCRM/Migrations/201602172308303_initial.Designer.cs
+++ CoderCampsCRM/CoderCampsCRM/Migrations/201602172308303_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace CoderCampsCRM.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class new_tasks : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(new_tasks));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201602172300524_new_tasks"; }
+            get { return "201602172308303_initial"; }
         }
         
         string IMigrationMetadata.Source
--- CoderCampsCRM/CoderCampsCRM/ngApp/app.js
+++ CoderCampsCRM/CoderCampsCRM/ngApp/app.js
@@ -167,3 +167,4 @@ var MyApp;
         $httpProvider.interceptors.push('authInterceptor');
     });
 })(MyApp || (MyApp = {}));
+//# sourceMappingURL=app.js.map
