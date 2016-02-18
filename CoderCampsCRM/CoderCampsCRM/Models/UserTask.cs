5da89d41dbfd7f5e3f3932fe5ebbdcae0117af78
mcollins2618 <mcollins2618@gmail.com>
2016-02-17 23:54:15 -0800

    config update

--- CoderCampsCRM/CoderCampsCRM/Models/UserTask.cs
+++ CoderCampsCRM/CoderCampsCRM/Models/UserTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,6 +15,9 @@ namespace CoderCampsCRM.Models
         public string Description { get; set; }
         public string Status { get; set; }
         public int? DealId { get; set; }
+        public string ContactId { get; set; }
+        [ForeignKey("ContactId")]
+        public Contact Contact { get; set; }
 
     }
 }
