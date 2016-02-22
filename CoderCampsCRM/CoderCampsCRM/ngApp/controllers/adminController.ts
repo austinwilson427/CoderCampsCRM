cc0e6e7db1655fe188b3683f39b706f669e15c31
Duran Gradwell <bandic22@gmail.com>
2016-02-22 18:34:04 +0000

    Added adminController.ts

---
+++ CoderCampsCRM/CoderCampsCRM/ngApp/controllers/adminController.ts
@@ -0,0 +1,13 @@
+﻿namespace MyApp.Controllers {
+
+    export class AdminController {
+
+        public users;
+        public sortType = 'firstName';
+        public sortReverse = false;
+
+        public constructor(private adminService: MyApp.Services.AdminService) {
+            this.users = this.adminService.getUserResource();
+        }
+    }
+}
