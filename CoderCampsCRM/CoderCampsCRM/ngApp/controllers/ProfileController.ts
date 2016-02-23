d478e5d25f29a51c14c4e1163d15040191a0210a
austinwilson427 <austin.wilson427@gmail.com>
2016-02-23 16:52:50 -0600

    Commit

--- CoderCampsCRM/CoderCampsCRM/ngApp/controllers/ProfileController.ts
+++ CoderCampsCRM/CoderCampsCRM/ngApp/controllers/ProfileController.ts
@@ -1,51 +1,51 @@
-﻿namespace MyApp.Controllers {
+﻿//namespace MyApp.Controllers {
 
-    export class ProfileController {
-        public profiles;
-        public profile;
-        public selectedProfile;
-        public validationErrors;
-        constructor(private profileService: MyApp.Services.AccountProfileService, private $location: ng.ILocaleService, private $route: ng.route.IRouteService) {
-            this.getProfile();
-        }
+//    export class ProfileController {
+//        public profiles;
+//        public profile;
+//        public selectedProfile;
+//        public validationErrors;
+//        constructor(private profileService: MyApp.Services.AccountProfileService, private $location: ng.ILocaleService, private $route: ng.route.IRouteService) {
+//            this.getProfile();
+//        }
 
-        getProfile() {
-            this.profileService.GetAccountProfile().then((result) => {
-                this.profile = result;
-            });
-        }
-        populateFields() {
-            this.profile = this.selectedProfile;
-        }
-        //add new profile here?
-        addProfile() {
-            this.validationErrors = [];
+//        getProfile() {
+//            this.profileService.GetAccountProfile().then((result) => {
+//                this.profile = result;
+//            });
+//        }
+//        populateFields() {
+//            this.profile = this.selectedProfile;
+//        }
+//        //add new profile here?
+//        addProfile() {
+//            this.validationErrors = [];
 
-            this.profile.isActive = true;
-            this.profile.Service.createAccountProfile(this.profile).then(() => {
-                this.$route.reload();
-                //}).catch(Error) => {
-                //    for (let i in Error.data.modelState[i];
-                //    this.validationErrors = this.validationErrors.concat(errorMessage);
+//            this.profile.isActive = true;
+//            this.profile.Service.createAccountProfile(this.profile).then(() => {
+//                this.$route.reload();
+//                //}).catch(Error) => {
+//                //    for (let i in Error.data.modelState[i];
+//                //    this.validationErrors = this.validationErrors.concat(errorMessage);
 
-                //}
+//                //}
 
-            });
-        }
-        editProfile() {
+//            });
+//        }
+//        editProfile() {
 
-            this.validationErrors = [];
-            this.profile.isActive = true;
-            this.profileService.saveAccountProfile(this.profile).then(() => {
-                this.$route.reload();
-            });
-        }
-        deleteProfile() {
-            this.profile.isActive = false;
-            this.profileService.saveAccountProfile(this.profile).then(() => {
-                this.$route.reload();
-            });
-        }
-    }
-}
+//            this.validationErrors = [];
+//            this.profile.isActive = true;
+//            this.profileService.saveAccountProfile(this.profile).then(() => {
+//                this.$route.reload();
+//            });
+//        }
+//        deleteProfile() {
+//            this.profile.isActive = false;
+//            this.profileService.saveAccountProfile(this.profile).then(() => {
+//                this.$route.reload();
+//            });
+//        }
+//    }
+//}
 
