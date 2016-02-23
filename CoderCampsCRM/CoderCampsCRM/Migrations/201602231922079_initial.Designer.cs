52356d04ed163888f91101218f23bd999f947654
bandic22 <bandic22@gmail.com>
2016-02-23 14:01:50 -0800

    DuranCommit

--- CoderCampsCRM/CoderCampsCRM/Migrations/201602231922079_initial.Designer.cs
+++ CoderCampsCRM/CoderCampsCRM/Migrations/201602231922079_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace CoderCampsCRM.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Initial : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201602221816229_initial"; }
+            get { return "201602231922079_initial"; }
         }
         
         string IMigrationMetadata.Source
