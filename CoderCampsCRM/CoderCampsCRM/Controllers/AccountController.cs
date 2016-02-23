053193959203b3b0b8ba6d27206900e34b4442e4
Guven Agas <guven.agas@outlook.com>
2016-02-22 20:49:46 -0800

    trial accountcontroller and identity model

--- CoderCampsCRM/CoderCampsCRM/Controllers/AccountController.cs
+++ CoderCampsCRM/CoderCampsCRM/Controllers/AccountController.cs
@@ -429,7 +429,7 @@ namespace CoderCampsCRM.Controllers
                 Company = model.Company,
                 PhoneNumber = model.PhoneNumber,
                 TimeZone = model.TimeZone,
-                PicUrl = model.PicUrl,
+               // PicUrl = model.PicUrl,
                 MemberSince = DateTime.Now,
             };
 
