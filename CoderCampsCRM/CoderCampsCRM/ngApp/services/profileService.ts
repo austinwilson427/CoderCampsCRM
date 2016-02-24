10c4e0a84c28c7b1be54b94ad09b23f0bc8e18eb
kim gorski <kimgorski24@gmail.com>
2016-02-24 01:20:07 +0000

    Added 3 files to services

---
+++ CoderCampsCRM/CoderCampsCRM/ngApp/services/profileService.ts
@@ -0,0 +1,24 @@
+﻿namespace MyApp.Services {
+////
+    export class AccountProfileService {
+      
+        private ProfileResource;
+        public profiles;
+
+        constructor($resource: ng.resource.IResourceService) {
+            this.ProfileResource = $resource("/api/profile");
+           
+        }
+
+        public GetAccountProfile() {
+               
+            return this.ProfileResource.get().$promise;
+        }
+        public saveAccountProfile(profile) {
+            return this.ProfileResource.save(profile).$promise;
+        }
+       
+
+    }
+    angular.module("MyApp").service("profileService",AccountProfileService);
+}
